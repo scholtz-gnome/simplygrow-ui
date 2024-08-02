3e42ffd1fa2b3ff3de963292584d77c12d8718b2
Stephen Scholtz <scholtz.gnome@gmail.com>
2024-08-02 16:34:02 +0200

    Add extract to rollup config for postcss plugin

---
+++ dist/components/index.d.ts
@@ -0,0 +1,3 @@
+export { default as FlexContainer } from './flex-container';
+export { default as Header } from './header';
+export { default as Heading } from './heading';
--- rollup.config.js
+++ rollup.config.js
@@ -18,6 +18,7 @@ module.exports = {
     commonjs(),
     postcss({
       modules: true,
+      extract: true,
     }),
   ],
 };
