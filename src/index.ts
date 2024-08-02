f655f9476442147a5a19bf72ce140b1febd05f10
Stephen Scholtz <scholtz.gnome@gmail.com>
2024-08-02 15:58:37 +0200

    Use global index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-export { default as FlexContainer } from './flex-container';
-export { default as Header } from './header';
-export { default as Heading } from './heading';
+export * from './components';
