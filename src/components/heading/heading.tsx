c1dfeb8c897149baa553bd1d27c5b0d922151794
Stephen Scholtz <scholtz.gnome@gmail.com>
2024-08-13 08:21:09 +0200

    Allow className to be passed down to Heading

--- src/components/heading/heading.tsx
+++ src/components/heading/heading.tsx
@@ -10,20 +10,23 @@ interface HeadingProps {
    */
   order?: 1 | 2 | 3;
   textAlign?: 'center';
+  className?: string;
   children: ReactNode;
 }
 
-const Heading: FC<HeadingProps> = ({ order = 1, textAlign, children }) => {
+const Heading: FC<HeadingProps> = ({ order = 1, textAlign, className, children }) => {
   const textAlignStyles = textAlign === 'center' ? styles.textAlign : undefined;
 
-  let headingElement: JSX.Element = <h1 className={`${styles.headingOne} ${textAlignStyles}`}>{children}</h1>;
+  let headingElement: JSX.Element = (
+    <h1 className={`${styles.headingOne} ${textAlignStyles} ${className}`}>{children}</h1>
+  );
 
   switch (order) {
     case 2:
-      headingElement = <h2 className={`${styles.headingTwo} ${textAlignStyles}`}>{children}</h2>;
+      headingElement = <h2 className={`${styles.headingTwo} ${textAlignStyles} ${className}`}>{children}</h2>;
       break;
     case 3:
-      headingElement = <h3 className={`${styles.headingThree} ${textAlignStyles}`}>{children}</h3>;
+      headingElement = <h3 className={`${styles.headingThree} ${textAlignStyles} ${className}`}>{children}</h3>;
       break;
   }
 
