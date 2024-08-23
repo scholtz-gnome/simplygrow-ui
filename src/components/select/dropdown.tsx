f0e47be9fdaae27ed8eddb4b06e308593620b5bc
Stephen Scholtz <scholtz.gnome@gmail.com>
2024-08-23 15:40:32 +0200

    Add theme styles to Select

--- src/components/select/dropdown.tsx
+++ src/components/select/dropdown.tsx
@@ -1,4 +1,5 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useContext } from 'react';
+import ThemeContext from '../../context';
 import styles from './dropdown.module.css';
 
 interface DropdownProps {
@@ -7,10 +8,29 @@ interface DropdownProps {
   isValid: boolean;
 }
 
-const Dropdown: FC<DropdownProps> = ({ focused, children, isValid }) => {
-  const validStyles = isValid ? styles.focused : styles.invalid;
+const Dropdown: FC<DropdownProps> = ({ focused, children }) => {
+  const theme = useContext(ThemeContext);
 
-  return <div className={`${styles.dropdown} ${focused ? validStyles : ''}`}>{children}</div>;
+  const focusedStyles = focused ? styles.focused : '';
+
+  let focusThemeStyles = '';
+
+  switch (theme) {
+    case 'peopleflow':
+      focusThemeStyles = styles.peopleflowThemeFocused;
+      break;
+    case 'worklight':
+      focusThemeStyles = styles.worklightThemeFocused;
+      break;
+    case 'skillbook':
+      focusThemeStyles = styles.skillbookThemeFocused;
+      break;
+    case undefined:
+      focusThemeStyles = styles.defaultThemeFocused;
+      break;
+  }
+
+  return <div className={`${styles.dropdown} ${focusedStyles} ${focused ? focusThemeStyles : ''}`}>{children}</div>;
 };
 
 export default Dropdown;
