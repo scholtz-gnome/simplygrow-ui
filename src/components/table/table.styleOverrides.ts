c3ae2f5521e76b34d26ac1c180cf9c8e60ba1ed6
Stephen Scholtz <scholtz.gnome@gmail.com>
2025-03-18 14:03:01 +0200

    Release v0.7.23

--- src/components/table/table.styleOverrides.ts
+++ src/components/table/table.styleOverrides.ts
@@ -47,5 +47,41 @@ export const styleOverrides = {
     },
   },
   skillbook: {},
-  worklight: {},
+  worklight: {
+    '.MuiDataGrid-columnHeader': {
+      backgroundColor: 'rgb(49, 60, 90)',
+      color: 'white',
+    },
+    '.MuiDataGrid-columnHeaderTitle': {
+      fontWeight: 550,
+    },
+    '.MuiIconButton': {
+      color: 'white',
+    },
+    '.MuiDataGrid-columnHeaderTitleContainerContent .MuiCheckbox-root': {
+      color: 'white',
+    },
+    '.MuiDataGrid-columnHeader .MuiSvgIcon-root': {
+      color: 'white',
+    },
+    '.MuiDataGrid-cell .MuiSvgIcon-root': {
+      fill: 'rgb(49, 60, 90)',
+    },
+    '.MuiDataGrid-columnSeparator .MuiDataGrid-columnSeparator--sideRight': {
+      display: 'none',
+    },
+    '.MuiDataGrid-virtualScrollerContent': {
+      color: 'rgb(150,150,175)',
+      fontWeight: 500,
+    },
+    '& .MuiDataGrid-cell:hover': {
+      cursor: 'pointer',
+    },
+    [`& .${gridClasses.cell}:focus, & .${gridClasses.cell}:focus-within`]: {
+      outline: 'none',
+    },
+    [`& .${gridClasses.columnHeader}:focus, & .${gridClasses.columnHeader}:focus-within`]: {
+      outline: 'none',
+    },
+  },
 };
