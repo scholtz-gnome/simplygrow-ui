c82010239f2e36ccbc663562be864fee70e335d7
NickiBleeker <nicki@peopleflow.co.za>
2025-03-20 13:27:17 +0200

    set & reuse variable

--- src/components/search-filter/clear.icon.tsx
+++ src/components/search-filter/clear.icon.tsx
@@ -2,24 +2,30 @@
 //
 
 type ClearIconProps = {
+  fillColour?: string;
   onClick: () => void;
 };
 
-export const ClearIcon = (props: ClearIconProps) => (
-  <div onClick={props.onClick}>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="18"
-      height="18"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <line x1="18" y1="6" x2="6" y2="18" fill="#878B93"></line>
-      <line x1="6" y1="6" x2="18" y2="18" fill="#878B93"></line>
-    </svg>
-  </div>
-);
+const ClearIcon = (props: ClearIconProps) => {
+  const fillColour = props.fillColour ?? '#878B93';
+  return (
+    <div onClick={props.onClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <line x1="18" y1="6" x2="6" y2="18" fill={fillColour}></line>
+        <line x1="6" y1="6" x2="18" y2="18" fill={fillColour}></line>
+      </svg>
+    </div>
+  );
+};
+
+export default ClearIcon;
