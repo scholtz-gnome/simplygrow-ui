33afb3a5f4c5cbf3353ea79fcaf77fb252eba233
Stephen Scholtz <scholtz.gnome@gmail.com>
2025-05-05 16:01:58 +0200

    Uses useEffect

--- src/hooks/use-pathname.hook.ts
+++ src/hooks/use-pathname.hook.ts
@@ -1,12 +1,14 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const usePathname = () => {
   const [path, setPath] = useState("");
 
-  if (typeof window !== "undefined") {
-    setPath(window.location.pathname);
-  }
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setPath(window.location.pathname);
+    }
+  }, []);
 
   return path;
 };
