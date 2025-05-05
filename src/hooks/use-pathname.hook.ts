cb8954f8d3c1dea12d901f3dd3b98b2a14d3c563
Stephen Scholtz <scholtz.gnome@gmail.com>
2025-05-05 15:58:40 +0200

    Simplifies hook

--- src/hooks/use-pathname.hook.ts
+++ src/hooks/use-pathname.hook.ts
@@ -1,28 +1,12 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const usePathname = () => {
-  const [path, setPath] = useState(typeof window !== "undefined" ? window.location.pathname : "");
+  const [path, setPath] = useState("");
 
-  useEffect(() => {
-    const handleLocationChange = () => {
-      if (typeof window !== "undefined") {
-        setPath(window.location.pathname);
-      }
-    };
-
-    if (typeof window !== "undefined") {
-      window.addEventListener("popstate", handleLocationChange);
-      window.addEventListener("pushstate", handleLocationChange);
-    }
-
-    return () => {
-      if (typeof window !== "undefined") {
-        window.removeEventListener("popstate", handleLocationChange);
-        window.removeEventListener("pushstate", handleLocationChange);
-      }
-    };
-  }, []);
+  if (typeof window !== "undefined") {
+    setPath(window.location.pathname);
+  }
 
   return path;
 };
